9dbb4836ca486d211dc9dffe5b133bcfc07f19c9
Sasikanth G <bgumpana@in.ibm.com>
2022-02-06 16:48:34 +0530

    Module for Data Virtualization Service on Cloud Pak for Data
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -35,37 +35,38 @@ for COMPONENT_NAME in $COMPONENT_NAMES; do
 
   echo "Printing payload/${LAYER}/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
   cat "payload/${LAYER}/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
+
+  count=0
+  until kubectl get namespace "${NAMESPACE}" 1>/dev/null 2>/dev/null || [[ $count -eq 20 ]]; do
+    echo "Waiting for namespace: ${NAMESPACE}"
+    count=$((count + 1))
+    sleep 15
+  done
+
+  if [[ $count -eq 20 ]]; then
+    echo "Timed out waiting for namespace: ${NAMESPACE}"
+    exit 1
+  else
+    echo "Found namespace: ${NAMESPACE}. Sleeping for 30 seconds to wait for everything to settle down"
+    sleep 30
+  fi
+
+  DEPLOYMENT="${COMPONENT_NAME}-${BRANCH}"
+  count=0
+  until kubectl get deployment "${DEPLOYMENT}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
+    echo "Waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
+    count=$((count + 1))
+    sleep 15
+  done
+
+  if [[ $count -eq 20 ]]; then
+    echo "Timed out waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
+    kubectl get all -n "${NAMESPACE}"
+    exit 1
+  fi
+
+  kubectl rollout status "deployment/${DEPLOYMENT}" -n "${NAMESPACE}" || exit 1
 done
-count=0
-until kubectl get namespace "${NAMESPACE}" 1>/dev/null 2>/dev/null || [[ $count -eq 20 ]]; do
-  echo "Waiting for namespace: ${NAMESPACE}"
-  count=$((count + 1))
-  sleep 15
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for namespace: ${NAMESPACE}"
-  exit 1
-else
-  echo "Found namespace: ${NAMESPACE}. Sleeping for 30 seconds to wait for everything to settle down"
-  sleep 30
-fi
-
-DEPLOYMENT="${COMPONENT_NAME}-${BRANCH}"
-count=0
-until kubectl get deployment "${DEPLOYMENT}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
-  echo "Waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
-  count=$((count + 1))
-  sleep 15
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
-  kubectl get all -n "${NAMESPACE}"
-  exit 1
-fi
-
-kubectl rollout status "deployment/${DEPLOYMENT}" -n "${NAMESPACE}" || exit 1
 
 cd ..
 rm -rf .testrepo
