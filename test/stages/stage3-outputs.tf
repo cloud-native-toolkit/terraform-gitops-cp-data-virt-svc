866a0a7e46d3453202628b05ab4616b8be239c53
Sasikanth G <bgumpana@in.ibm.com>
2022-02-07 13:20:45 +0530

    updated the dependencies
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,15 +5,15 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        sub_chart   = module.gitops_module.sub_chart
-        inst_chart  = module.gitops_module.inst_chart
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name        = module.cp-data-virtualization.name
+        sub_chart   = module.cp-data-virtualization.sub_chart
+        inst_chart  = module.cp-data-virtualization.inst_chart
+        branch      = module.cp-data-virtualization.branch
+        namespace   = module.cp-data-virtualization.namespace
+        server_name = module.cp-data-virtualization.server_name
+        layer       = module.cp-data-virtualization.layer
+        layer_dir   = module.cp-data-virtualization.layer == "infrastructure" ? "1-infrastructure" : (module.cp-data-virtualization.layer == "services" ? "2-services" : "3-applications")
+        type        = module.cp-data-virtualization.type
       })
     }
   }
