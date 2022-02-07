b21a4c765c8b1aede3ff3cf3a95300d314c337b0
Sasikanth G <bgumpana@in.ibm.com>
2022-02-07 13:18:20 +0530

    updated the dependencies
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage2-cpd-dv.tf
+++ test/stages/stage2-cpd-dv.tf
@@ -1,4 +1,4 @@
-module "gitops_module" {
+module "cp-data-virtualization" {
   source = "./module"
 
   gitops_config = module.gitops.gitops_config
@@ -6,7 +6,6 @@ module "gitops_module" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
-  # catalog = module.gitops_ibm_catalogs.catalog_ibmoperators
   # operator_namespace= module.cp4d-operator.namespace
   cpd_namespace = module.cp4d-instance.namespace
 }
